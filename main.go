4e8df81dc7a22b2abf2f26663960d06b84a7b5af
Ashwin Rajeev <ashwinrajeev916@gmail.com>
2019-02-22 17:02:23 +0530

    custom input timer implemented using go routines

--- main.go
+++ main.go
@@ -8,12 +8,17 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 )
 
 // Quiz is a abstract struct for storing
 // the quiz information.
 type Quiz struct {
+	Timer             *time.Timer
+	CustomInputTime   time.Duration
 	QuestionAndAnswer map[string]string
+	TotalQuestions    int
+	QuestionsAnswered int
 	Marks             int
 	Right             int
 	Wrong             int
@@ -24,18 +29,34 @@ type Quiz struct {
 func newQuiz() *Quiz {
 	return &Quiz{
 		QuestionAndAnswer: make(map[string]string, 100),
+		Timer:             time.NewTimer(5 * time.Minute),
 	}
 }
 
 func main() {
 	q := newQuiz()
-	file := flag.String("file", "problems.csv", "give the path of input csv file")
+	file := flag.String("file", "problems.csv", "Give the path of input csv file")
+	t := flag.Int("time", 30, "Give custom game duration time(In Seconds)")
 	flag.Parse()
+	s := fmt.Sprintf("%ds", *t)
+	duration, err := time.ParseDuration(s)
+	if err != nil {
+		fmt.Println(err)
+	}
+	q.CustomInputTime = duration
 	q.readFromCSV(*file)
+	q.TotalQuestions = len(q.QuestionAndAnswer)
+	q.userInterface()
+}
+
+func (q *Quiz) userInterface() {
 	var input int
 	for {
+		if !q.Timer.Reset(5 * time.Minute) {
+			fmt.Println("Timer reseted failed")
+		}
 		fmt.Printf("\n\t\tWelcome to Online quiz \n\n")
-		fmt.Printf("**********************************************\n")
+		fmt.Printf("********************************************\n")
 		fmt.Printf("\n\t\t1.Start quiz\n")
 		fmt.Printf("\t\t2.Exit\n\n")
 		fmt.Printf("\t\tEnter your choice:\n\n")
@@ -64,7 +85,6 @@ func (q *Quiz) readFromCSV(filename string) {
 		for j, l := range k {
 			if j == 0 {
 				q.QuestionAndAnswer[l] = records[i][j+1]
-				continue
 			}
 		}
 	}
@@ -73,10 +93,35 @@ func (q *Quiz) readFromCSV(filename string) {
 // quizGame game is a function where the
 // actual game logic is taken place.
 func (q *Quiz) quizGame() {
+	var input, flag string
 	q.Marks = 0
 	q.Right = 0
 	q.Wrong = 0
-	var input string
+e:
+	fmt.Println("Shall we start the game? (y/n)")
+	fmt.Scan(&flag)
+	if flag != "y" && flag != "n" {
+		fmt.Println("Invalid entry")
+		goto e
+	} else if flag == "n" {
+		return
+	}
+	if q.Timer.Reset(q.CustomInputTime) {
+		fmt.Printf("\nYour time start now...You have total of %d Seconds", q.CustomInputTime/1000000000)
+	}
+	// Creating a go routine for checking the time
+	go func() {
+		select {
+		case <-q.Timer.C:
+			q.Timer.Stop()
+			fmt.Println("\nSorry time out, Please try again later!!!")
+			fmt.Printf("\nTotal questions: %d , You Answered: %d", q.TotalQuestions, q.QuestionsAnswered)
+			fmt.Printf("\nRight answers: %d, Wrong answers: %d", q.Right, q.Wrong)
+			fmt.Println("\n\tYour total points: ", q.Marks)
+			os.Exit(0)
+		}
+	}()
+
 	for question, answer := range q.QuestionAndAnswer {
 	k:
 		fmt.Printf("\nwhat is the result of %s : ", question)
@@ -94,12 +139,15 @@ func (q *Quiz) quizGame() {
 			fmt.Println("Right answer!")
 			q.Right++
 			q.Marks++
+			q.QuestionsAnswered++
 			continue
 		}
 		q.Wrong++
+		q.QuestionsAnswered++
 		fmt.Println("Wrong answer!")
 		fmt.Printf("Right answer is %s \n", answer)
 	}
+	fmt.Printf("\nTotal questions: %d , You Answered: %d", q.TotalQuestions, q.QuestionsAnswered)
 	fmt.Printf("\nRight answers: %d, Wrong answers: %d", q.Right, q.Wrong)
-	fmt.Printf("\n\tYour total points: %d", q.Marks)
+	fmt.Println("\n\tYour total points: ", q.Marks)
 }
